11ad66aa7d0252daa7936bb4f43c34d0561d5a13
trantanminh <trantanminh0603@gmail.com>
2025-07-10 23:36:56 +0700

    allow at 1 image

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -490,10 +490,7 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    image: Schema.Attribute.Media<
-      'images' | 'files' | 'videos' | 'audios',
-      true
-    > &
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'> &
       Schema.Attribute.SetPluginOptions<{
         i18n: {
           localized: true;
